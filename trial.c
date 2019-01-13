90a552fcca60a0783b36acc14a5bd4647651e432
Oishi Chowdhury <oishic1997@gmail.com>
2019-01-13 21:55:14 +0530

    trial

---
+++ trial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%.0f\n", 0.5);
+    printf("%.0f\n", 100.5004004);
+    printf("%.0f\n", 100.0);
+    printf("%.0f\n", 10.9999);
+    printf("%.0f\n", 9999.9999);
+
+    printf("%.0f\n", -0.5);
+    printf("%.0f\n", -100.5004004);
+    printf("%.0f\n", -100.0);
+    printf("%.0f\n", -10.9999);
+    printf("%.0f\n", -9999.9999);
+
+    return 0;
+}
